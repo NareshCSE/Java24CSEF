87767acf7a2c46643d67b37ce873f40ebbe75721
24wh1a05cc <24wh1a05cc@bvrithyderabad.edu.in>
2025-08-20 11:50:00 +0530

    Create PatternD.java

---
+++ 24WH1A05CC/PatternD.java
@@ -0,0 +1,24 @@
+package patternd;
+
+public class Patternd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			int j=1;
+			for(j=1;j<=(5-i);j++) {
+				System.out.print("  ");
+			}
+			int k=1;
+			for(k=1;k<=i;k++) {
+				System.out.print(k+" ");
+			}
+			for(int n=i-1;n>=1;n--) {
+				System.out.print(n+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
