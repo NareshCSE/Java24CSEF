c9dd45c7b8f3db7c895efbdc29176ec7a3ae598b
24WH1A05Z3 <24wh1a05z3@bvrithyderabad.edu.in>
2025-08-20 12:02:09 +0530

    Create Patternrightpyramid.java

---
+++ 24WH1A05Z3/Patternrightpyramid.java
@@ -0,0 +1,19 @@
+package assignment_problems;
+
+public class Pattern2second {
+
+	public static void main(String[] args) {
+		int row=5;
+		for(int i=row;i>0;i--) {
+			for(int j=1;j<i;j++) {
+				System.out.print("- ");
+			}
+			for(int k=row;k>=i;k--) {
+				System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
