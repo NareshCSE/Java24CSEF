a31c8353b835b538c77ff48d3286647f946bd07a
Mounika-540 <pentanimounika1@gmail.com>
2025-08-20 11:43:11 +0530

    Create calculator.java

---
+++ 25WH5A0540/calculator.java
@@ -0,0 +1,27 @@
+package electricity;
+public class calculater {
+
+	public static void main(String[]args)
+	{
+		int a =10;
+		int b =20;
+		int choice=3;
+		switch(choice)
+		{
+		case 1:
+			System.out.print(a+b);
+			break;
+		case 2:
+			System.out.println(a-b);
+			break;
+		case 3:
+			System.out.println(a*b);
+			break;
+		case 4:
+			System.out.println(a/b);
+			break;
+		}
+		
+	}
+
+}
