50aa961c6baf732a70d0750eb5e0cedc4d389412
24wh1a05z1 <24wh1a05z1@bvrithyderabad.edu.in>
2025-08-20 12:01:40 +0530

    24WH1A05Z1: Added InvertTriangle.java

---
+++ InvertTriangle.java
@@ -0,0 +1,22 @@
+package assignment1;
+import java.util.Scanner;
+
+public class InvertTriangle {
+
+	public static void main(String[] args) {
+		int i, j, k;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n value:");
+		int n = sc.nextInt();
+		for(i = 1; i <= n; i++) {
+			for(k = n - i; k > 0; k--) {
+				System.out.print(" ");
+			}
+			for(j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
