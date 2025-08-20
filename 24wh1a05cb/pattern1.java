911339d0e50b846b25a46e4dc995a346fa59b7a8
Challa Krushitha <24wh1a05cb@bvrithyderabad.edu.in>
2025-08-20 11:08:06 +0530

    Create pattern1.java

---
+++ 24wh1a05cb/pattern1.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class rectangularpattern {
+
+	public static void main(String[] args) {
+		int i,j,n=5;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
