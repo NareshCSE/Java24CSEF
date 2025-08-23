0f49727c5fe873151d4299c4071ada025be47de3
Rithvika615 <banothrithvika56@gmail.com>
2025-08-23 09:45:27 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class evenodd {
+	public static void main(String[] args) {
+		for(int i =1;i<= 100;i++) {
+			if(i%2 == 0) {
+				System.out.println(i +" Even number");
+			}
+			else {
+				System.out.println(i + " Odd number");
+			}
+		}
+		}
+	}
