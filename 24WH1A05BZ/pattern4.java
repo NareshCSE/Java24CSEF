12b128d5597a4c724dfa7620ebe7bc4eec6bf0ad
geethika626 <24wh1a05bz@bvrithyderabad.edu.in>
2025-09-03 11:16:12 +0530

    Create pattern4.java

---
+++ 24WH1A05BZ/pattern4.java
@@ -0,0 +1,26 @@
+package java2;
+
+public class pattern4 {
+	public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		int j=1;
+		for(j=1;j<=(5-i);j++)
+		{
+		System.out.print("  ");
+		}
+		int k=1;
+		for(k=1;k<=i;k++)
+		{
+		System.out.print(k+" ");
+		}
+		for(int n=i-1;n>=1;n--)
+		{
+		System.out.print(n+" ");
+		}
+		System.out.println();
+	}
+
+
+}
+}
