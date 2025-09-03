94bee8b976d918ca3d92f8fa5752f74e8da1a220
24WH1A05Z3 <24wh1a05z3@bvrithyderabad.edu.in>
2025-09-03 11:50:57 +0530

    Update Patternrightpyramid.java

--- 24WH1A05Z3/Patternrightpyramid.java
+++ 24WH1A05Z3/Patternrightpyramid.java
@@ -6,7 +6,7 @@ public class Pattern2second {
 		int row=5;
 		for(int i=row;i>0;i--) {
 			for(int j=1;j<i;j++) {
-				System.out.print("- ");
+				System.out.print("  ");
 			}
 			for(int k=row;k>=i;k--) {
 				System.out.print("* ");
