d2fb4cd8c6c041cebb66898993eeb4e1e0081146
Hamsa057 <24wh1a05aj@bvrithyderabad.edu.in>
2025-09-03 11:09:04 +0530

    DuplicatesInArray.java

---
+++ 24wh1a05aj/DuplicatesInArray.java
@@ -0,0 +1,25 @@
+package basicprogrames;
+import java.util.Scanner;
+public class ArraysDuplicateValues {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter number of integers:");
+		int n=sc.nextInt();
+		int numbers[]=new int[n];
+		for(int i=0;i<n;i++) {
+			System.out.print("Enter numbers " + i + ":");
+			numbers[i]=sc.nextInt();
+		}
+		System.out.println("Duplicates are:");
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<i;j++) {
+				if(numbers[i]==numbers[j]) {
+					System.out.println(numbers[i]);
+				}
+			}
+		}
+		sc.close();
+	}
+
+}
