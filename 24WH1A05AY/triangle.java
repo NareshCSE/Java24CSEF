45bda1c7408af7958f42ff1b88e6ffda6fb77395
Bhavana598 <24wh1a05ax@bvrithyderabad.edu.in>
2025-09-03 11:06:01 +0530

    Create triangle.java

---
+++ 24WH1A05AY/triangle.java
@@ -0,0 +1,20 @@
+package javaprog;
+import java.util.Scanner;
+public class Triangle_pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner number=new Scanner(System.in);
+		System.out.println("enter number");
+		int n=number.nextInt();
+		int i,j;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
