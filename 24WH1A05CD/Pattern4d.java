e901e985dcbc0b7abe392c5c364808fdfa9b85e1
24WH1A05CD <24wh1a05cd@bvrithyderabad.edu.in>
2025-09-03 11:27:15 +0530

    Create Pattern4d.java

---
+++ 24WH1A05CD/Pattern4d.java
@@ -0,0 +1,27 @@
+package javalab;
+
+public class pattern4d {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print(k);
+			}
+			for(int l=2;l<=i;l++)
+			{
+				
+				System.out.print(i-l+1);
+			}
+			System.out.println();
+		}
+		
+	}
+	
+}
