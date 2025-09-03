34016ec8c332f47c8c7a17606fbc10de4d7aa6de
Challa Krushitha <24wh1a05cb@bvrithyderabad.edu.in>
2025-09-03 11:43:43 +0530

    Create pattern4.java

---
+++ 24wh1a05cb/pattern4.java
@@ -0,0 +1,23 @@
+package patternc;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5-i;j++) {
+				System.out.print(" ");
+			}
+			int k=1;
+			for(k=1;k<=i;k++) {
+				System.out.print(k+"");
+			}
+			for(int n=i-1;n>=1;n--) {
+				System.out.print(n+"");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
