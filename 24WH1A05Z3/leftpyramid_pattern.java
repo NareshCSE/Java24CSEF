bcd501517d3a9c7af22f53c4c71bc2bb9e43c0af
24WH1A05Z3 <24wh1a05z3@bvrithyderabad.edu.in>
2025-09-03 11:52:23 +0530

    Update and rename rightinvertedpyramid_pattern.java to leftpyramid_pattern.java

--- 24WH1A05Z3/leftpyramid_pattern.java
+++ 24WH1A05Z3/leftpyramid_pattern.java
@@ -1,15 +1,12 @@
 package assignment_problems;
 
-public class Pattern2 {
+public class Pattern2second {
 
 	public static void main(String[] args) {
 		int row=5;
 		for(int i=1;i<=row;i++) {
-			for(int j=(row-i);j>0;j--) {
-				System.out.print(" ");
-			}
-			for(int k=1;k<=i;k++) {
-				System.out.print("*");
+			for(int j=0;j<i;j++) {
+				System.out.print("* ");
 			}
 			System.out.println(" ");
 		}
