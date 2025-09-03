3d2b02c4c424092b20c1257e57caad3a6ef00263
24wh1a05ar-art <24wh1a05ar@bvrithyderabad.edu.in>
2025-09-03 10:03:27 +0530

    Create Factorial.java

---
+++ 24WH1A05AR/Factorial.java
@@ -0,0 +1,19 @@
+package projectsAR;
+
+class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int number=5;
+        int fact=1;
+        int i;
+        for(i=5;i>=1;i--) {
+        	fact=fact*i;
+        	
+        	
+        	
+        }
+        System.out.println(fact);
+	}
+
+}
