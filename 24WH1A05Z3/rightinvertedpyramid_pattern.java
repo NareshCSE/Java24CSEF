db7711e76bf4d0108d3f7dcddad4fd6e5878d38a
24WH1A05Z3 <24wh1a05z3@bvrithyderabad.edu.in>
2025-09-03 11:48:03 +0530

    Create rightinvertedpyramid_pattern.java

---
+++ 24WH1A05Z3/rightinvertedpyramid_pattern.java
@@ -0,0 +1,19 @@
+package assignment_problems;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		int row=5;
+		for(int i=1;i<=row;i++) {
+			for(int j=(row-i);j>0;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
