f610a6b94bbc56a4511d5be606a59f7425082706
24WH1A05-CG <24wh1a05cg@bvrithyderabad.edu.in>
2025-09-03 12:05:42 +0530

    Create Pattern4c.java

---
+++ 24WH1A05CG/Pattern4c.java
@@ -0,0 +1,38 @@
+package sahasra;
+
+public class Pattern_4c {
+
+	public static void main(String[] args) {
+		int n = 9;
+		for (int i = 0; i < n; i++)
+   {
+			for (int j = 0; j < n; j++)
+      {
+				if (i > (n - 1) / 2) 
+         {
+					if (j >= ((n - 1) / 2) - (n - 1 - i) && j <= ((n - 1) / 2) + (n - 1 - i))
+          {
+						System.out.print("*");
+					} 
+          else {
+						System.out.print(" ");
+					}
+				} 
+        else
+         {
+					if (j >= ((n - 1) / 2) - i && j <= ((n - 1) / 2) + i)
+         {
+						System.out.print("*");
+
+					} 
+        else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
