5255e3bd23a15b1959af93ace083a4eb2ef31bc7
24wh1a05z1 <24wh1a05z1@bvrithyderabad.edu.in>
2025-09-03 12:12:21 +0530

    Added DuplicateArray.java

---
+++ 24WH1A05Z1/DuplicateArray.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class DuplicateArray {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter " + size + " integers:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Duplicate elements are: ");
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " "); 
+                    break;
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
