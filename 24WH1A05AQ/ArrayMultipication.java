00806b3915614f8bb5ec24ba40b537fe40dce582
Akshara1511 <24wh1a05aq@bvrithyderabad.edu.in>
2025-09-10 11:09:48 +0530

    Add files via upload

---
+++ 24WH1A05AQ/ArrayMultipication.java
@@ -0,0 +1,50 @@
+package cse05aq;
+
+public class ArrayMultipication {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter rows of first matrix: ");
+	        int m = sc.nextInt();
+	        System.out.print("Enter columns of first matrix / rows of second matrix: ");
+	        int n = sc.nextInt();
+	        System.out.print("Enter columns of second matrix: ");
+	        int p = sc.nextInt();
+
+	        int[][] A = new int[m][n];
+	        System.out.println("Enter elements of first matrix:");
+	        for (int i = 0; i < m; i++) {
+	            for (int j = 0; j < n; j++) {
+	                A[i][j] = sc.nextInt();
+	            }
+	        }
+	        int[][] B = new int[n][p];
+	        System.out.println("Enter elements of second matrix:");
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < p; j++) {
+	                B[i][j] = sc.nextInt();
+	            }
+	        }
+
+	        int[][] C = new int[m][p];
+
+	        for (int i = 0; i < m; i++) {
+	            for (int j = 0; j < p; j++) {
+	                C[i][j] = 0;
+	                for (int k = 0; k < n; k++) {
+	                    C[i][j] += A[i][k] * B[k][j];
+	                }
+	            }
+	        }
+
+	        System.out.println("Resultant Matrix:");
+	        for (int i = 0; i < m; i++) {
+	            for (int j = 0; j < p; j++) {
+	                System.out.print(C[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
+
+	        sc.close();
+	    }
+}
