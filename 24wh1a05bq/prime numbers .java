894341df4c8f78cf7c089f5e1a41c19b69920148
24wh1a05bq-pixel <24wh1a05bq@bvrithyderabad.edu.in>
2025-09-10 12:04:57 +0530

    Create prime numbers .java

---
+++ 24wh1a05bq/prime numbers .java
@@ -0,0 +1,38 @@
+package projects;
+import java.util.Scanner;
+public class primenumbers {
+		    // Function to check prime
+		    public static boolean isPrime(int num) {
+		        if (num <= 1) return false;
+		        for (int i = 2; i * i <= num; i++) {
+		            if (num % i == 0) return false;
+		        }
+		        return true;
+		    }
+
+		    public static void main(String[] args) {
+		        Scanner sc = new Scanner(System.in);
+
+		        System.out.print("Enter n: ");
+		        int n = sc.nextInt();
+
+		        int a = 0, b = 1;
+
+		        System.out.print("Prime numbers in Fibonacci series up to " + n + ": ");
+
+		        // Generate Fibonacci numbers until n
+		        while (a <= n) {
+		            if (isPrime(a)) {
+		                System.out.print(a + " ");
+		            }
+		            int c = a + b;
+		            a = b;
+		            b = c;
+		        }
+		        sc.close();
+		    }
+		
+
+	
+
+}
