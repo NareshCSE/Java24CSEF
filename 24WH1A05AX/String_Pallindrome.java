610a50406eb99166f253781b14470ebdbb5ed402
Bhavana598 <24wh1a05ax@bvrithyderabad.edu.in>
2025-10-17 16:09:09 +0530

    String_palindrome
    Refactor palindrome check to ignore spaces and case sensitivity

--- 24WH1A05AX/String_Pallindrome.java
+++ 24WH1A05AX/String_Pallindrome.java
@@ -1,22 +1,32 @@
-package javaprog;
-
+package myjava;
 import java.util.Scanner;
-
-public class Pallindrome {
+public class exp7 {
 
 	public static void main(String[] args) {
-		Scanner input=new Scanner(System.in);
-		System.out.println("enter a string");
-		String str=input.nextLine();
-		
-		StringBuilder reverse=new StringBuilder(str);
-		reverse.reverse();
-		
-		if(str.equals(reverse.toString())) {
-			System.out.println("String is Pallindrome");
-		}else {
-			System.out.println("String is not a Pallindrome");
-		}
-			
+		Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+
+        
+        if (str.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is NOT a palindrome.");
+        }
+
+        sc.close();
+
 	}
+
 }
