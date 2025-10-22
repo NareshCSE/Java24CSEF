0c1f8e22966bd3109a75df43a680ff22993d6ea7
Hamsa057 <24wh1a05aj@bvrithyderabad.edu.in>
2025-10-22 11:05:28 +0530

    Exp16PinMisMatch.java

---
+++ 24wh1a05aj/Exp16PinMisMatch.java
@@ -0,0 +1,48 @@
+package basicprogrames;
+import java.util.Scanner;
+
+class PinMisMatch extends Exception{
+	public PinMisMatch() {
+		System.out.println("Exception Caught: Incorrect Pin. Please try again");
+	}
+}
+
+public class Exp16AtmPinException {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        int correctPin = 1234;   
+        int attempts = 0;
+        
+        System.out.print("Enter your name:");
+        String accHolderName=sc.next();
+        
+        while(attempts<3) {
+		System.out.print("Enter your pin:");
+		int pin = sc.nextInt();
+		
+		try {
+			if(correctPin==pin) {
+				System.out.println("Welcome "+ accHolderName + " access granted!");
+				break;
+			}
+			else {
+				throw new PinMisMatch();
+			}
+		}
+	    catch(PinMisMatch e){
+	    }
+		
+		attempts++;
+        }
+        
+        if(attempts>=3) {
+        	System.out.println("Too many attempts. Sorry your account has been locked.");
+        }
+		
+        sc.close();
+
+	}
+
+}
