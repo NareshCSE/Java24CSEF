cca3beb5401dcc677486bb866b003f70c39b1a66
Rithvika615 <banothrithvika56@gmail.com>
2025-10-26 11:04:33 +0530

    Update calculator.java

--- 25wh5a0541/calculator.java
+++ 25wh5a0541/calculator.java
@@ -1,26 +1,39 @@
-package demo;
-public class calculator{
-	public static void main (String[] args) {
-		int a=100;
-		int b=2000;
-		int choice=5;
-		switch(choice) {
-		case 1:
-			System.out.println(a+b);
-			break;
-		case 2:
-			System.out.println(a-b);
-			break;
-		case 3:
-			System.out.println(a*b);
-			break;
-		case 4:
-			System.out.println(a/b);
-			break;
-		case 5:
-			System.out.println(a%b);
-			break;
+import java.util.Scanner;
+public class calculator {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter first number: ");
+		double num1=sc.nextDouble();
+		System.out.print("Enter second number: ");
+		double num2=sc.nextDouble();
+		System.out.print("Enter operator: ");
+		char op=sc.next().charAt(0);
+		double result;
+		switch (op){
+		case '+':
+		result=num1+num2;
+		break;
+		case '-':
+		result=num1-num2;
+		break;
+		case '*':
+		result=num1*num2;
+		break;
+		case '/':
+		if (num2!=0)
+		result=num1/num2;
+		else {
+		System.out.println("Error: Division by zero!");
+		sc.close();
+		return;
 		}
-	}
+		break;
+		default:
+		System.out.println("Invalid operator!");
+		sc.close();
+		return;
+		}
+		System.out.println("Result = " + result);
+		sc.close();
+		}
 }
-
