c263a6f9df5a19e2d9d595cce65556a3e5b35bc9
Rithvika615 <banothrithvika56@gmail.com>
2025-10-26 10:51:54 +0530

    Update powerbill.java

--- 25wh5a0541/powerbill.java
+++ 25wh5a0541/powerbill.java
@@ -1,24 +1,26 @@
-package demo;
-
-public class powerbill {
+import java.util.Scanner;
+public class power {
 	public static void main(String[] args) {
-		int units=120;
-		if(units<=50) {
-			System.out.println(units*1);
-		}
-			else if(units<=100) {
-				System.out.println(50*1 +(units-50)*2);
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter old reading: ");
+			int oldReading=sc.nextInt();
+			System.out.print("Enter current reading: ");
+			int currentReading=sc.nextInt();
+			int units=currentReading-oldReading;
+			double amount = 0;
+			if(units<=50) {
+			amount=units*1;
+			} else if(units<=100) {
+			amount=(50*1)+(units - 50)*2;
+			} else if(units<=200){
+			amount=(50*1)+(50*2)+(units-100)*3;
+			} else if(units<=400) {
+			amount=(50*1)+(50*2)+(100*3)+(units-200)*4;
+			} else{
+			amount=(50*1)+(50*2)+(100*3)+(200*4)+(units-400)*5;
 			}
-			else if(units<=200) {
-				System.out.println(50*1+50*2 +(units-100)*3);
+			System.out.println("Units Consumed: " + units);
+			System.out.println("Electricity Bill: Rs." + amount);
+			sc.close();
 			}
-			else if(units<=400) {
-				System.out.println(50*1+50*2+50*3+(units-200)*4);
-			}
-			else{
-				System.out.println(50*1+50*2+50*3+50*4+(units-400)*5);
-			}
-	}
 }
-	
-
