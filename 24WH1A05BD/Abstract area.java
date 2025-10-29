fc95590693badfb883d112e7a84fb2f9fc1f33c1
Gopika1206 <24wh1a05bd@bvrithyderabad.edu.in>
2025-10-29 10:12:46 +0530

    Create Abstract area.java

---
+++ 24WH1A05BD/Abstract area.java
@@ -0,0 +1,109 @@
+package java2025;
+
+
+
+import java.util.Scanner;
+abstract class Shape {
+
+    int a, b; 
+
+    abstract void printArea(); 
+}
+class Rectangle extends Shape {
+
+    Rectangle(int length, int breadth) {
+
+        a = length;
+
+        b = breadth;
+
+    }
+
+
+
+    void printArea() {
+
+        System.out.println("Rectangle Area: " + (a * b));
+
+    }
+
+}
+class Triangle extends Shape {
+
+    Triangle(int base, int height) {
+
+        a = base;
+
+        b = height;
+
+    }
+    void printArea() {
+
+        System.out.println("Triangle Area: " + (0.5 * a * b));
+
+    }
+
+}
+
+class Circle extends Shape {
+
+    Circle(int radius) {
+
+        a = radius;
+
+    }
+
+
+
+    void printArea() {
+
+        System.out.println("Circle Area: " + (Math.PI * a * a));
+
+    }
+
+}
+public class Ass13 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Shaps s;
+        System.out.print("Enter length of Rectangle: ");
+
+        int length = sc.nextInt();
+
+        System.out.print("Enter breadth of Rectangle: ");
+
+        int breadth = sc.nextInt();
+
+        s = new Rectangle(length, breadth);
+        s.printArea();
+        System.out.println();
+        System.out.print("Enter base of Triangle: ");
+
+        int base = sc.nextInt();
+
+        System.out.print("Enter height of Triangle: ");
+
+        int height = sc.nextInt();
+
+        s = new Triangle(base, height);
+
+        s.printArea();
+        System.out.println();
+        System.out.print("Enter radius of Circle: ");
+
+        int radius = sc.nextInt();
+
+        s = new Circle(radius);
+
+        s.printArea();
+
+
+
+        sc.close();
+
+    }
+
+}
