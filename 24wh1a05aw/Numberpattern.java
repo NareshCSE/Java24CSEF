b6ec3baa5c301c9049e8fb49816879af772b7091
Manasa <24wh1a05aw@bvrithyderabad.edu.in>
2025-10-29 10:46:12 +0530

    Numberpattern.java

---
+++ 24wh1a05aw/Numberpattern.java
@@ -0,0 +1,21 @@
+package csef5aw;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,space;
+		for (i = 1; i <= 5; i++) {
+		    space = 6 - i;
+		    for (j = 1; j <= space; j++)
+		        System.out.print(" ");
+		    for (j = 1; j <= i; j++)
+		        System.out.print(j);
+		    for (j = i-1; j >= 1; j--) {
+                System.out.print(j );
+            }
+		    System.out.println();
+		}
+	}
+
+	}
