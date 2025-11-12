a4646465ce1ba2a907f7edcb2bf7b1f2f3771e28
Hamsa057 <24wh1a05aj@bvrithyderabad.edu.in>
2025-11-12 12:05:28 +0530

    Exp20ProducerConsumer.java

---
+++ 24wh1a05aj/Exp20ProducerConsumer.java
@@ -0,0 +1,81 @@
+package basicprogrames;
+
+class ItemQueue{
+    private int number;
+
+	public synchronized void produce(int num) {
+        this.number = num;
+        System.out.println("Produced: " + num);
+        notify(); 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized int consume() {
+        System.out.println("Consumed: " + number);
+        notify(); 
+        try {
+            Thread.sleep(1000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return number;
+    }
+}
+
+class Produce extends Thread {
+    private ItemQueue item;
+
+    public Produce(ItemQueue item) {
+        this.item = item;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) { 
+            synchronized (item) {
+                item.produce(i);
+                try {
+                    item.wait(); 
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+class Consume extends Thread {
+    private ItemQueue item;
+
+    public Consume(ItemQueue item) {
+        this.item = item;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) { 
+            synchronized (item) {
+                item.consume();
+                try {
+                    item.wait(); 
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+public class Exp20Producerconsumer {
+    public static void main(String[] args) {
+        ItemQueue item = new ItemQueue();
+
+        Produce producer = new Produce(item);
+        Consume consumer = new Consume(item);
+
+        producer.start();
+        consumer.start();
+    }
+}
