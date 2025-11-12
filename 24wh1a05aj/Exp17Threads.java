14683de0e5d3d2e4599c2ada2e39673077c73339
Hamsa057 <24wh1a05aj@bvrithyderabad.edu.in>
2025-11-12 12:04:48 +0530

    Exp17Threads.java

---
+++ 24wh1a05aj/Exp17Threads.java
@@ -0,0 +1,63 @@
+package basicprogrames;
+
+import java.util.Random;
+
+class RandomNum extends Thread{
+	public void run() {
+		Random rand = new Random();
+		for(int i=0;i<5;i++) {
+			int num=rand.nextInt(100) ;
+			System.out.println("Generatednumber: " + num);
+			
+		if(num%2==0) {
+			EvenNum even = new EvenNum(num);
+			even.start();
+		}
+		else {
+			OddNum odd = new OddNum(num);
+			odd.start();
+		}
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+}
+
+class EvenNum extends Thread {
+	int num;
+	public EvenNum(int num) {
+		this.num=num;
+	}
+	
+	public void run() {	
+		int sum = num*num;
+		System.out.println("Square of number:" + sum + "\n");
+	}
+}
+
+class OddNum extends Thread {
+	int num;
+	public OddNum(int num) {
+		this.num=num;
+	}
+	
+	public void run() {
+		int sum = num*num*num;
+		System.out.println("Cube of number:" + sum + "\n");
+	}
+}
+
+public class Exp17Threads {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		RandomNum rn = new RandomNum();
+		rn.start();
+		
+		}
+}
