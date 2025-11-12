be01f965e12b4a070b3c24b246f2280f4cbee6ab
Charvi Parmar <24wh1a05bt@bvrithyderabad.edu.in>
2025-11-12 09:44:14 +0530

    Create NumberGeneratorThread.java

---
+++ 24WH1A05BT/NumberGeneratorThread.java
@@ -0,0 +1,41 @@
+package javaPrograms;
+import java.util.*;
+class NumGenerator extends Thread{
+	public void run() {
+		Random r = new Random();
+		int num = r.nextInt(50);
+		System.out.println("Generated number : " + num);
+		if (num % 2 == 0) {
+			new EvenThread(num).start();
+		}else {
+			new OddThread(num).start();
+		}
+	}
+}
+
+class EvenThread extends Thread{
+	int num;
+	EvenThread(int n){
+		num = n;
+	}
+	public void run() {
+		System.out.println("Square of " + num + " = " + num*num);
+	}
+}
+
+class OddThread extends Thread{
+	int num;
+	OddThread(int n){
+		num = n;
+	}
+	public void run() {
+		System.out.println("Cube of "+ num + " = " + num*num);
+	}
+}
+
+public class RandomNumberThread {
+	public static void main(String[] args) {
+		NumGenerator t1 = new NumGenerator();
+		t1.start();
+	}
+}
