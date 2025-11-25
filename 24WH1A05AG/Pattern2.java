dff1f37dca76599af2a056e75b637801f45a0ca3
Shaveta564 <24wh1a05ag@bvrithyderabad.edu.in>
2025-11-25 19:59:25 +0530

    Update Pattern2.java

--- 24WH1A05AG/Pattern2.java
+++ 24WH1A05AG/Pattern2.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
-public class Week1_triangle_pattern1 {
-
+public class Reverse_Triangle_Pattern {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the number of rows in pattern:");
-		int rows=sc.nextInt();
-		int i,j,s;
-		for(i=1;i<=rows;i++)
-		{
-			for(s=1;s<=rows-i;s++)
-			{
-				System.out.print("  ");
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter number:");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int s=1;s<=n-i;s++) {
+				System.out.print(" ");
 			}
-			for(j=1;j<=i;j++)
-			{
-				System.out.print(" *");
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
 			}
-		System.out.println();
+			System.out.println();
 		}
 		sc.close();
-		
 	}
-
 }
+
