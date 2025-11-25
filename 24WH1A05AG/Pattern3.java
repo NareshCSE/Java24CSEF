02a745661650c95db10123a237246115f86ea64f
Shaveta564 <24wh1a05ag@bvrithyderabad.edu.in>
2025-11-25 19:59:58 +0530

    Update Pattern3.java

--- 24WH1A05AG/Pattern3.java
+++ 24WH1A05AG/Pattern3.java
@@ -1,43 +1,42 @@
 import java.util.Scanner;
-public class Week1_diamondPattern {
+public class Diamond_Pattern {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.print("enter the no. of rows:");
-		int rows=sc.nextInt();
-		int i,j,s;
-		for(i=1;i<=rows;i++)
-		{
-			for(s=1;s<=rows-i;s++)
+		        Scanner sc=new Scanner(System.in);
+		        System.out.print("Enter the number of rows:");
+		        int n=sc.nextInt();
+		        for(int i=1;i<=n;i++)
+		       {
+			for(int s=1;s<=n-i;s++)
 			{
-				System.out.print("  ");
+			      System.out.print("  ");
 			}
-			for(j=1;j<=i;j++)
+			for(int j=1;j<=i;j++)
 			{
-				System.out.print("* ");
-			}
-			for(int k=i-1;k>=1;k--)
-			{
-				System.out.print("* ");
-			}
-			System.out.println();
-		}
-		for(i=rows-1;i>=1;i--)
-		{
-			for(s=1;s<=rows-i;s++)
-			{
-				System.out.print("  ");
-			}
-			for(j=1;j<=i;j++)
-			{
-				System.out.print("* ");
+			      System.out.print("* ");
 			}
 			for(int k=i-1;k>=1;k--)
 			{
-				System.out.print("* ");
+			      System.out.print("* ");
+			}
+		            System.out.println();
+		       }
+		       for(int i=n-1;i>=1;i--)
+		      {
+			for(int s=1;s<=n-i;s++)
+			{
+			      System.out.print("  ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+			      System.out.print("* ");
+			}
+			for(int k=i-1;k>=1;k--)
+			{
+			      System.out.print("* ");
 			}
 			System.out.println();
-		}
-		sc.close();
-			
-	   }
+		       }
+		       sc.close();
 	}
+}
+
