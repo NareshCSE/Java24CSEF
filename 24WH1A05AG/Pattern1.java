e0f4422cb1a815b7965824007884e0b63c2a6b8c
Shaveta564 <24wh1a05ag@bvrithyderabad.edu.in>
2025-11-25 19:55:42 +0530

    Update Pattern1.java

--- 24WH1A05AG/Pattern1.java
+++ 24WH1A05AG/Pattern1.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
-public class Week1_triangle_pattern {
-
+public class Triangle_Pattern {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.print("enter the numbers of rows of the pattern:");
-		int rows=sc.nextInt();
-		int i,j;
-		for(i=1;i<=rows;i++)
-		{
-			for(j=1;j<=i;j++)
-			{
-				System.out.print("*  ");
+		Scanner number=new Scanner(System.in);
+		System.out.print("Enter number:");
+		int n=number.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
 			}
-			System.out.println();
+            System.out.println();
 		}
-		sc.close();
-
 	}
-
 }
+
+
