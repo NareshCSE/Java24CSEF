1b2482d7e9aadea167c5fd0e0f2732e4fb4da45f
Shaveta564 <24wh1a05ag@bvrithyderabad.edu.in>
2025-11-25 19:58:53 +0530

    Update Sum_of_even_odd.java

--- 24WH1A05AG/Sum_of_even_odd.java
+++ 24WH1A05AG/Sum_of_even_odd.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-public class Week1_Sumof_even_odd {
-
+public class Sum_Of_Even_Odd {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		int i,Oddsum=0,Evensum=0;
 		System.out.print("Enter how many integers you want to enter:");
 		int n=sc.nextInt();
+		System.out.print("Enter " + n + " integer: ");
 		for(i=1;i<=n;i++)
 		{
-			System.out.print("Enter " + i + " integer: ");
 			int num=sc.nextInt();
 			if(num%2==0)
 			{
@@ -24,5 +22,7 @@ public class Week1_Sumof_even_odd {
 		System.out.println("Even sum is " + Evensum);
 		sc.close();
 	}
+}
+
 
 }
