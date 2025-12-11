28f9d72cf29a6f906bdb83592f17908e4a8ca2a7
upputurisanjana <24wh1a05z3@bvrithyderabad.edu.in>
2025-12-11 22:55:19 +0530

    Refactor palindrome check using a loop

--- 24WH1A05Z3/string_palindrome.java
+++ 24WH1A05Z3/string_palindrome.java
@@ -1,26 +1,26 @@
 package assignment_problems;
-
 import java.util.Scanner;
 
 public class String_palindrome {
-
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		String str = new String();
-		System.out.println("Enter a string:");
-        str = scanner.nextLine();
-        StringBuilder str1 = new StringBuilder(str);
-        String reversestr = str1.reverse().toString();
-
-        if(str.equals(reversestr)){
-        	System.out.println("given string is a palindrome");
-        }
-        else {
-        	System.out.println("string is not a palindrome");
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = scanner.nextLine();
+        
+        String reversestr = "";
+        
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversestr += str.charAt(i);
         }
         
-
-	}
-
+        if(str.equals(reversestr)) {
+            System.out.println("given string is a palindrome");
+        }
+        else {
+            System.out.println("string is not a palindrome");
+        }
+        
+        scanner.close();
+    }
 }
